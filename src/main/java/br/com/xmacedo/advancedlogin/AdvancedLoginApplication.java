7bde76ca4f306f328f52d32b135b6df91794f631
Felipe Macedo <felipe.x.macedo@gmail.com>
2026-01-02 18:25:01 -0300

    Creating structure for the project

---
+++ src/main/java/br/com/xmacedo/advancedlogin/AdvancedLoginApplication.java
@@ -0,0 +1,13 @@
+package br.com.xmacedo.advancedlogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdvancedLoginApplication.class, args);
+    }
+
+}
